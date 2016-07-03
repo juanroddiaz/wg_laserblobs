c23574728120bed5e5c27e4097197ec27598a939
Juan Rodrigo Diaz <juan.rod.diaz@gmail.com>
2016-07-02 20:27:26 -0400

    Setting basic on hold behaviour for pivots-- It doesn't detect the outside area hold correctly, TO CHECK!

--- Assets/01_scripts/Scenes/MainScene/BattleGroundPivotLogic.cs
+++ Assets/01_scripts/Scenes/MainScene/BattleGroundPivotLogic.cs
@@ -3,5 +3,13 @@ using UnityEngine.EventSystems;
 
 public class BattleGroundPivotLogic : MonoBehaviour
 {
+    public void OnHoldStart()
+    {
+        CustomLog.Log("OnHoldStart");
+    }
 
+    public void OnHoldEnd()
+    {
+        CustomLog.Log("OnHoldEnd");
+    }
 }
