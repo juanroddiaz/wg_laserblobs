985612798286c7144bccce944d82b894ce6da255
Juan Rodrigo Diaz <juan.rod.diaz@gmail.com>
2016-07-03 17:47:24 -0400

    Adds more battleground and laser logic. Sets main laser images and some PHs.

---
+++ Assets/01_scripts/Scenes/MainScene/Battle/LaserBeamLogic.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LaserBeamLogic : MonoBehaviour
+{
+    [SerializeField]
+    private Image _enemyLaserImage;
+    [SerializeField]
+    private Image _playerLaserImage;
+}
