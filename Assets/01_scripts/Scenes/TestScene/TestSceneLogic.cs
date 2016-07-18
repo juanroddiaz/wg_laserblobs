e44e9bcbbd85805307a75d9b1198bc4083855d6c
Juan Rodrigo Diaz <juan.rod.diaz@gmail.com>
2016-07-17 23:07:06 -0400

    Blob test scene improvements.

---
+++ Assets/01_scripts/Scenes/TestScene/TestSceneLogic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSceneLogic : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _blobAnimController;
+
+    public void SetIdle()
+    {
+        _blobAnimController.SetTrigger("Idle");
+    }
+
+    public void SetJump()
+    {
+        _blobAnimController.SetTrigger("Jump");
+    }
+
+    public void SetHold()
+    {
+        _blobAnimController.SetTrigger("Press");
+    }
+}
