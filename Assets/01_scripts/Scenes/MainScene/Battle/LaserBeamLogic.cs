99ba9ae229f9a24d0dcd39b23f43b2218a77b276
Juan Rodrigo Diaz <juan.rod.diaz@gmail.com>
2016-07-24 21:03:53 -0400

    Enables collision particle position logic.

--- Assets/01_scripts/Scenes/MainScene/Battle/LaserBeamLogic.cs
+++ Assets/01_scripts/Scenes/MainScene/Battle/LaserBeamLogic.cs
@@ -13,8 +13,8 @@ public class LaserBeamLogic : MonoBehaviour
     [SerializeField]
     private List<ParticleSystem> _collisionGlow;
     [SerializeField]
-    private GameObject _collisionGlowObj;
-
+    private Transform       _collisionGlowTransform;
+        
     private ParticleSystemRenderer _playerLaserRenderer;
     private ParticleSystemRenderer _enemyLaserRenderer;
 
@@ -22,6 +22,9 @@ public class LaserBeamLogic : MonoBehaviour
     private float _currentPlayerLaserValue = 0.0f;
     private float _currentPlayerLaserForceFactor = 0.0f;
     private float _totalLaserValue = 0.0f;
+    private float _enemyTipPos = 0.0f;
+    private float _totalDistanceTipPos = 0.0f;
+    private Vector3 _collisionPosition;
 
     public void Init(float enemyForce, float playerForce, float lenght, float enemyTipPos, float playerTipPos)
     {
@@ -34,7 +37,12 @@ public class LaserBeamLogic : MonoBehaviour
         _enemyLaserRenderer = _enemyLaser.GetComponent<ParticleSystemRenderer>();
         _playerLaserRenderer.lengthScale = 0.5f * lenght;
         _enemyLaserRenderer.lengthScale = 0.5f * lenght;
-    }
+
+        _enemyTipPos = enemyTipPos;
+        _totalDistanceTipPos = enemyTipPos-playerTipPos;
+        _collisionPosition = _collisionGlowTransform.localPosition;
+        SetCollisionObjectPosition();
+}
 
     // TODO: get the enemy and player type too, it will have an impact in laser force
     public void UpdateLaserLane(float enemyForce, float playerForce)
@@ -44,6 +52,14 @@ public class LaserBeamLogic : MonoBehaviour
         _currentPlayerLaserValue = Mathf.Min(_currentPlayerLaserValue, 1.0f);
         _playerLaserRenderer.lengthScale = _currentPlayerLaserValue * _totalLaserValue;
         _enemyLaserRenderer.lengthScale = _totalLaserValue * (1.0f - _currentPlayerLaserValue);
+        SetCollisionObjectPosition();
         _currentPlayerLaserForceFactor = enemyForce - playerForce;
+    }
+
+    private void SetCollisionObjectPosition()
+    {
+        float pos = _enemyTipPos - (_totalDistanceTipPos) * (1.0f - _currentPlayerLaserValue);
+        _collisionPosition.x = pos;
+        _collisionGlowTransform.localPosition = _collisionPosition;
     }
 }
