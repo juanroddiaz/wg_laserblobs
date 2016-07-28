270dcbbcf0f8c72ff9308cae58ee1af9c2dc9a4c
Leo <dhazguten@gmail.com>
2016-07-28 00:28:08 -0300

    Animations configured for Front view. Squeeze and Fire actions separated by states. Animations renamed. Buttons rearranged in scene. Back view pending.

--- Assets/01_scripts/Scenes/TestScene/TestSceneLogic.cs
+++ Assets/01_scripts/Scenes/TestScene/TestSceneLogic.cs
@@ -6,48 +6,93 @@ public class TestSceneLogic : MonoBehaviour
     [SerializeField]
     private Animator _blobAnimController;
 
-    public void SetIdleFront()
+    public void SetIdle_F()
     {
-        _blobAnimController.SetTrigger("IdleFront");
+        _blobAnimController.SetTrigger("Idle_F");
     }
 
-    public void SetJumpFront()
+	public void SetIdle_B()
+	{
+		_blobAnimController.SetTrigger("Idle_B");
+	}
+
+    public void SetJump_F()
     {
-        _blobAnimController.SetTrigger("JumpFront");
+        _blobAnimController.SetTrigger("Jump_F");
     }
 
-    public void SetHoldFront()
+	public void SetJump_B()
+	{
+		_blobAnimController.SetTrigger("Jump_B");
+	}
+
+	public void SetSqueeze_F()
     {
-        _blobAnimController.SetTrigger("SqueezeFront");
+		_blobAnimController.SetTrigger("Squeeze_F");
     }
 
-	public void SetIdleBack()
+	public void SetSqueeze_B()
 	{
-		_blobAnimController.SetTrigger("IdleBack");
+		_blobAnimController.SetTrigger("Squeeze_B");
 	}
 
-	public void SetJumpBack()
+	public void SetSqueezeIn_F()
 	{
-		_blobAnimController.SetTrigger("JumpBack");
+		_blobAnimController.SetTrigger("SqueezeIn_F");
 	}
 
-	public void SetHoldBack()
+	public void SetSqueezeIn_B()
 	{
-		_blobAnimController.SetTrigger("SqueezeBack");
+		_blobAnimController.SetTrigger("SqueezeIn_B");
 	}
 
-	public void SetGradient()
+	public void SetSqueezeOut_F()
 	{
-		_blobAnimController.SetTrigger("ColorGradient");
+		_blobAnimController.SetTrigger("SqueezeOut_F");
 	}
 
-	public void SetDeathFront()
+	public void SetSqueezeOut_B()
 	{
-		_blobAnimController.SetTrigger("DeathFront");
+		_blobAnimController.SetTrigger("SqueezeOut_B");
 	}
 
-	public void SetFireFront()
+	public void SetDeath_F()
 	{
-		_blobAnimController.SetTrigger("FireFront");
+		_blobAnimController.SetTrigger("Death_F");
+	}
+
+	public void SetDeath_B()
+	{
+		_blobAnimController.SetTrigger("Death_B");
+	}
+
+	public void SetFire_F()
+	{
+		_blobAnimController.SetTrigger("Fire_F");
+	}
+
+	public void SetFire_B()
+	{
+		_blobAnimController.SetTrigger("Fire_B");
+	}
+
+	public void SetFireIn_F()
+	{
+		_blobAnimController.SetTrigger("FireIn_F");
+	}
+
+	public void SetFireIn_B()
+	{
+		_blobAnimController.SetTrigger("FireIn_B");
+	}
+
+	public void SetFireOut_F()
+	{
+		_blobAnimController.SetTrigger("FireOut_F");
+	}
+
+	public void SetFireOut_B()
+	{
+		_blobAnimController.SetTrigger("FireOut_B");
 	}
 }
