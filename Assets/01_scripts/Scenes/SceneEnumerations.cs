1f44b019bf72ad2f391fe9c67776d2b2fc2d9a45
Juan Rodrigo Diaz <juan.rod.diaz@gmail.com>
2016-07-28 18:53:31 -0400

    Adding missing file.

---
+++ Assets/01_scripts/Scenes/SceneEnumerations.cs
@@ -0,0 +1,33 @@
+﻿public enum BlobTypes
+{
+    RED = 0,
+    BLUE,
+    YELLOW,
+    ORANGE,
+    PURPLE,
+    GREEN,
+    GRAY,
+    WHITE,
+    BLACK,
+    MAX
+}
+
+public enum BlobAnimations
+{
+
+}
+
+public enum GameDifficulty
+{
+    Easy = 0,
+    Medium,
+    Hard,
+    Max
+}
+
+public enum BattleGroundType
+{
+    PLAYER = 0,
+    ENEMY,
+    MAX
+}
