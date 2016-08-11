9adbd936965c5ae906b54dbf4de892a50622fbbb
Juan Rodrigo Diaz <juan.rod.diaz@gmail.com>
2016-08-10 23:03:57 -0400

    Initilizes affinityConfiguration scriptable object.

---
+++ Assets/01_scripts/Scenes/MainScene/Battle/Affinity/AffinityConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class BlobSetting
+{
+    public BlobTypes type = BlobTypes.MAX;
+    public List<BlobAffinities> affinities = new List<BlobAffinities>();
+}
+
+public class AffinityRelationship
+{
+    public BlobAffinities affinity = BlobAffinities.MAX;
+    public float percentage = 100.0f;
+}
+
+[System.Serializable]
+public class AffinitySetting
+{
+    public BlobAffinities affinity = BlobAffinities.MAX;
+    public List<AffinityRelationship> affinities = new List<AffinityRelationship>();
+}
+
+public class AffinityConfiguration : ScriptableObject
+{
+    public List<BlobSetting> blobsSetting = new List<BlobSetting>();
+    public List<AffinitySetting> affinitySetting = new List<AffinitySetting>();    
+}
---
+++ Assets/01_scripts/Scenes/MainScene/Battle/Affinity/Editor/AffinityConfCreator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class AffinityConfCreator : MonoBehaviour
+{
+    [MenuItem("Tools/Create New Affinity Configuration")]
+    private static void CreateNewAffinityConfiguration()
+    {
+        AffinityConfiguration ac = ScriptableObject.CreateInstance<AffinityConfiguration>();
+
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        if (path == "")
+        {
+            path = "Assets";
+        }
+        else if (Path.GetExtension(path) != "")
+        {
+            path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+        }
+
+        string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/" + typeof(AffinityConfiguration).ToString() + ".asset");
+
+        AssetDatabase.CreateAsset(ac, assetPathAndName);
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = ac;
+
+        CustomLog.LogWarning("A new instance of AffinityConfiguration was created!");
+    }
+}
