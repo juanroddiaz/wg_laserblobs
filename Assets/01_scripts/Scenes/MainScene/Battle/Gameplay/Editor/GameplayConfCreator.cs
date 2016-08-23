dd14d36691af4c35f451a5bb5c96cdf8d8bca9a8
Juan Rodrigo Diaz <juan.rod.diaz@gmail.com>
2016-08-22 21:15:29 -0300

    Adding gameplay configuration as scriptable object.

---
+++ Assets/01_scripts/Scenes/MainScene/Battle/Gameplay/Editor/GameplayConfCreator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class GameplayConfCreator : MonoBehaviour
+{
+    [MenuItem("Tools/Create New Gameplay Configuration")]
+    private static void CreateNewGameplayConfiguration()
+    {
+        GameplayConfiguration ac = ScriptableObject.CreateInstance<GameplayConfiguration>();
+
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        if (path == "")
+        {
+            path = "Assets";
+        }
+        else if (Path.GetExtension(path) != "")
+        {
+            path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+        }
+
+        string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/" + typeof(GameplayConfiguration).ToString() + ".asset");
+
+        AssetDatabase.CreateAsset(ac, assetPathAndName);
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = ac;
+
+        CustomLog.LogWarning("A new instance of GameplayConfCreator was created!");
+    }
+}
---
+++ Assets/01_scripts/Scenes/MainScene/Battle/Gameplay/GameplayConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class BlobsWaveConfig
+{
+    public int  startTurn;
+    public bool isRandomPool;
+    public List<BlobTypes> availableTypes = new List<BlobTypes>();
+}
+
+public class GameplayConfiguration : ScriptableObject
+{
+    public int amountForBlobEarning = 5;
+    public int amountForDifficultIncreasing = 5;
+    public List<BlobsWaveConfig> blobsWaveConfig = new List<BlobsWaveConfig>();
+}
