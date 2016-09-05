c828fb421da6f99dd02e8bd573cdba5c18c83460
Juan Rodrigo Diaz <juan.rod.diaz@gmail.com>
2016-09-04 21:33:57 -0300

    Sets initial blob types list in gamePlayConfig. Sets a new way to set waves' duration in gameplayConfig.

--- Assets/01_scripts/Scenes/MainScene/Battle/Gameplay/GameplayConfiguration.cs
+++ Assets/01_scripts/Scenes/MainScene/Battle/Gameplay/GameplayConfiguration.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
+/// <summary>
+/// Blob wave configuration 
+/// </summary>
 [System.Serializable]
 public class BlobsWaveConfig
 {
-    public int  startTurn;
+    // a random pool will pick any blob from the pool with a normal random selection. A non random will pick the next blob by list sorting
     public bool isRandomPool;
+    // hoy many random entries this wave will have. For a non random wave, the entries will be as same as the list amount
+    public int randomIterations = 0;
+    // available blob types for this wave configuration
     public List<BlobTypes> availableTypes = new List<BlobTypes>();
 }
 
 public class GameplayConfiguration : ScriptableObject
 {
+    // amount of blobs killed to get another blob as reward from availableTypes pool
     public int amountForBlobEarning = 5;
+    // amount of blobs killed to get a difficulty increment
     public int amountForDifficultIncreasing = 5;
+    // starting blob types
+    public List<BlobTypes> startingTypes = new List<BlobTypes>();
+    // list of blob waves configuration for gameplay
     public List<BlobsWaveConfig> blobsWaveConfig = new List<BlobsWaveConfig>();
 }
